8c24eb5dc76db6e2eff7f728e889db33dc080f5b
Alexandre Silva <agtech.dsv@gmail.com>
2026-06-26 08:20:12 -0300

    feat: add ProgressBarProvider to display navigation progress via route transitions and link interception

--- src/components/ProgressBarProvider.tsx
+++ src/components/ProgressBarProvider.tsx
@@ -106,7 +106,7 @@ function ProgressBarInner() {
           initial={{ opacity: 0 }}
           animate={{ opacity: 1 }}
           exit={{ opacity: 0, transition: { delay: 0.1, duration: 0.2 } }}
-          className="fixed top-0 left-0 right-0 h-[3px] z-[99999] pointer-events-none"
+          className="fixed top-0 left-0 right-0 h-[5px] z-[99999] pointer-events-none"
         >
           {/* Progress Line */}
           <motion.div
