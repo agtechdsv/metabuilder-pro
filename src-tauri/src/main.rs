a6c68a3e1e30ee6d0b2d0340f811d7c1141cadf8
Alexandre Silva <agtech.dsv@gmail.com>
2026-07-02 19:16:24 -0300

    feat: initialize Tauri application entry point and configure default security capabilities

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,16 +1,6 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
-// #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 fn main() {
-    std::panic::set_hook(Box::new(|info| {
-        eprintln!("Ocorreu um erro fatal (Panic):");
-        eprintln!("{:#?}", info);
-        eprintln!("======================================");
-        eprintln!("Por favor, copie ou tire uma foto desta tela!");
-        eprintln!("Pressione ENTER para fechar...");
-        let mut input = String::new();
-        let _ = std::io::stdin().read_line(&mut input);
-    }));
-
     app_lib::run();
 }
