ea7a0fe27cdeadbc6eead09f3179d8be539cbfe4
Alexandre Silva <agtech.dsv@gmail.com>
2026-07-02 17:43:06 -0300

    Reordenação da inicialização dos plugins

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -78,7 +78,6 @@ fn status_cli(state: State<'_, CliState>) -> Result<bool, String> {
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
-        .plugin(tauri_plugin_updater::Builder::new().build())
         .plugin(tauri_plugin_single_instance::init(|app, args, _cwd| {
             if let Some(window) = app.get_webview_window("main") {
                 let _ = window.unminimize();
@@ -90,6 +89,7 @@ pub fn run() {
                 let _ = app.emit("deep-link://new-url", vec![url.clone()]);
             }
         }))
+        .plugin(tauri_plugin_updater::Builder::new().build())
         .plugin(tauri_plugin_deep_link::init())
         .plugin(tauri_plugin_shell::init())
         .plugin(tauri_plugin_opener::init())
